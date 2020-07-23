96fe1683881ec623feb35e11523acff3982afc6f
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-07-23 16:23:59 +0300

    Initial Commit

---
+++ SwiftUIMenus/SwiftUIMenusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMenusApp.swift
+//  SwiftUIMenus
+//
+//  Created by ramil on 23.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMenusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
