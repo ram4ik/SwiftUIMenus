9341262dc2fd2de5649adbbbaadab8aaff537ce8
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-07-23 16:31:31 +0300

    init

--- SwiftUIMenus/ContentView.swift
+++ SwiftUIMenus/ContentView.swift
@@ -9,8 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!").padding()
+        VStack {
+            Menu("Options") {
+                Button("Do something", action: one)
+                Button("Try something", action: two)
+                
+                Menu("Another Options") {
+                    Button("Do something again", action: one)
+                    Button("Try something again", action: two)
+                }
+                Button("Cancel", action: cancel)
+            }
+            
+            Menu {
+                Button("Do something again", action: one)
+                Button("Try something again", action: two)
+            } label: {
+                Label("Options", systemImage: "paperplane")
+            }
+        }
     }
+    
+    func one() {}
+    func two() {}
+    func cancel() {}
 }
 
 struct ContentView_Previews: PreviewProvider {
